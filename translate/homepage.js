32f4f541867a2238c6a3a134909d206744ab6f45
YanJi <63778412+YanShuHao314@users.noreply.github.com>
2022-02-21 20:51:19 +0800

    Create homepage.js

---
+++ translate/homepage.js
@@ -0,0 +1,11 @@
+SIMS_TRANSL={
+  //主页菜单部分
+	"account":["我的账户","My account"],
+	"accountmsg":["点击登录","click here to login"],
+	"qq":["加入QQ交流群","Join our QQ group"],
+	"wechat":["关注微信公众号","Subscribe to Wechat"],
+	"links":["友情链接","Friends"],
+	"tos":["服务条款","Terms of service"],
+	"more":["站长的更多作品","More works"],
+	"about":["关于氢软","About SimSoft"],
+}
