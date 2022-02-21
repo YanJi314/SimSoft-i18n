a917e78b1baccf44f75fb00443bf20cace1c5cad
YanJi <63778412+YanShuHao314@users.noreply.github.com>
2022-02-21 21:31:41 +0800

    Update homepage.js

--- translate/homepage.js
+++ translate/homepage.js
@@ -10,5 +10,18 @@ SIMS_TRANSL={
 	"about":["关于氢软","About SimSoft"],
   //对话框内文案
 	"404":["您刚才尝试访问的地址不存在，所以您已被重定向到此页面。如果您认为这是一个错误，请<a href='//account.simsoft.top/?a=feedback'>向我们发送反馈</a>。很抱歉为您带来不便，感谢您对氢软的支持。","The page you've requested was not found. If this is an error, <a href='//account.simsoft.top/?a=feedback'>click here</a> to report an issue."],
-	"aboutdialog":["<b>SimSoft 丨 氢软</b><br><br>由<a href='//cloudflare.com'>Cloudflare</a>提供攻击防御服务<br>由<a href='//vercel.com'>Vercel</a>提供加速服务<br>由<a href='//starxn.com'>星辰云</a>提供网页托管<br><br>开发者：YSH（YanJi）<br>对本站的一切盗版行为将受到追究。<br><br>Based on SimUI 4.0.","<b>SimSoft 丨 氢软</b><br><br>DDoS protection by <a href='//cloudflare.com'>Cloudflare</a>.<br>CDN by <a href='//vercel.com'>Vercel</a>.<br>Web hosting by <a href='//starxn.com'>Starxn Cloud</a>.<br><br>Developer：YSH（YanJi）<br>Copyright © SimSoft.<br><br>Based on SimUI 4.0."]
+	"aboutdialog":["<b>SimSoft 丨 氢软</b><br><br>由<a href='//cloudflare.com'>Cloudflare</a>提供攻击防御服务<br>由<a href='//vercel.com'>Vercel</a>提供加速服务<br>由<a href='//starxn.com'>星辰云</a>提供网页托管<br><br>开发者：YSH（YanJi）<br>对本站的一切盗版行为将受到追究。<br><br>Based on SimUI 4.0.","<b>SimSoft 丨 氢软</b><br><br>DDoS protection by <a href='//cloudflare.com'>Cloudflare</a>.<br>CDN by <a href='//vercel.com'>Vercel</a>.<br>Web hosting by <a href='//starxn.com'>Starxn Cloud</a>.<br><br>Developer：YSH（YanJi）<br>Copyright © SimSoft.<br><br>Based on SimUI 4.0."],
+  //应用列表
+	"simtab":["氢起始","SimTab"],
+	"simtab_desc":["简洁实用的新标签页","Useful NewTab"],
+	"simmusic":["氢音乐","SimMusic"],
+	"simmusic_desc":["全网音乐免费解析","Free Music"],
+	"simimg":["氢图床","SimIMG"],
+	"simimg_desc":["免费高速CDN图床","Image Storage"],
+	"simurl":["氢短链","SimURL"],
+	"simurl_desc":["免费短链接服务","URL Shortener"],
+	"simvoice":["氢配音","SimVoice"],
+	"simvoice_desc":["在线AI配音制作","Text to Speech"],
+	"simtodo":["氢待办","SimTodo"],
+	"simtodo_desc":["高效在线待办清单","Online Todo-List"]
 }
