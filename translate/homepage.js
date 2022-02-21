e8c7e12d8e74b277a0b8d5a5bd8b601945925e9a
YanJi <63778412+YanShuHao314@users.noreply.github.com>
2022-02-21 21:06:03 +0800

    Update homepage.js

--- translate/homepage.js
+++ translate/homepage.js
@@ -8,4 +8,6 @@ SIMS_TRANSL={
 	"tos":["服务条款","Terms of service"],
 	"more":["站长的更多作品","More works"],
 	"about":["关于氢软","About SimSoft"],
+  //对话框内文案
+	"404":["您刚才尝试访问的地址不存在，所以您已被重定向到此页面。如果您认为这是一个错误，请<a href='//account.simsoft.top/?a=feedback'>向我们发送反馈</a>。很抱歉为您带来不便，感谢您对氢软的支持。","The page you've requested was not found. If this is an error, <a href='//account.simsoft.top/?a=feedback'>click here</a> to report an issue."]
 }
