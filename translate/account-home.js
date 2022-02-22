81246b15011a93332030788fe6c7b62326274418
YanJi <63778412+YanShuHao314@users.noreply.github.com>
2022-02-22 20:13:38 +0800

    Create account-home.js

---
+++ translate/account-home.js
@@ -0,0 +1,40 @@
+SIMS_TRANSL={
+  //主页面部分
+    "main-mail":["邮箱","Email Address"],
+    "main-pwd":["密码","Password"],
+    "main-exp":["我的积分","My Exp."],
+    "main-time":["注册时间","Registration"],
+    "main-app":["我的应用","Applications"],
+    "main-fb":["发送反馈","Feedback"],
+    "main-del":["删除账户","Delete Account"],
+    "main-exp-desc":["当前拥有 {num} 积分","You have {num} Exps."],
+    "main-app-desc-empty":["没有创建应用","No app found"],
+    "main-app-desc-num":["已创建 {num} 个应用","{num} app(s) created"],
+    "main-fb-desc":["报告问题或建议新功能","Report an issue here"],
+    "main-del-desc":["永久删除您的氢软账户","Delete your account forever"]
+  //顶部导航&菜单项目
+    "top-basic":["基本信息","Basic"],
+    "top-device":["登录设备","Devices"],
+    "menu-homepage":["返回主站","SimSoft homepage"],
+    "menu-tos":["服务条款","Terms of service"],
+    "menu-logout":["退出登录","Logout"],
+    "menu-about":["关于本站","About this page"],
+    "title":["氢软账户","My SimSoft Account"],
+  //消息框内容
+    "emailCopied":["邮箱复制到剪切板了呐 φ(*￣0￣)","Email address copied to clipboard."],
+    "logoutAll":["从所有设备退出登录","Logout from all devices"],
+    "logoutCurr":["若要在当前设备退出登录，请使用菜单中的『退出登录』功能_(:з」∠)_","Use 'logout' in the menu to logout from current device."],
+    "currDeviceTip":[" (当前设备)"," (Current)"],
+    "regTime":["您是氢软的第 {num} 位注册用户！<br>感谢您对氢软的支持[]~(￣▽￣)~*","You're the NO.{num} user of SimSoft."],
+  //用户反馈
+    "fb-success":["反馈发送了呐~ 我们会尽快回复。祝您有美好的一天 (≧∇≦)ﾉ","Feedback sent successfully."],
+    "fb-err":["发送反馈时遇到未知问题：<br>","Unknown error：<br>"],
+    "fb-qq":["通过QQ发送","QQ Group"],
+    "fb-web":["在线发送","Webpage"],
+    "fb-qq-desc":["加入官方群聊<br>获得快速解答","Chat with us."],
+    "fb-web-desc":["使用网页填写<br>便捷发送反馈","Send here."],
+    "fb-qq-btn":["复制群号","Copy ID"],
+    "fb-web-btn":["发送反馈","Select"],
+    "fb-cancel":["取消","Cencel"],
+    "fb-web-placeholder":["请在这里输入反馈内容呐~ 利用此功能发送无意义信息或骚扰客服将被永久封禁账号<( ￣^￣)>","Type your feedback..."]
+}
